d14aa55e6610188d12f46cdd36b454ac821e1f5d
Marc Boulet <24886455+MarcovChain@users.noreply.github.com>
2021-02-20 22:54:24 -0700

    added data table

--- dash/mortgage/mortgage_app.py
+++ dash/mortgage/mortgage_app.py
@@ -6,6 +6,7 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import dash_table
 import plotly.express as px
 import pandas as pd
 import numpy as np
@@ -28,14 +29,14 @@ ww = pd.read_csv(
 # row-based metrics
 # principal percentage and cumsum
 ww['prin%'] = np.where(ww['type'] == 'payment', ww['principal'] / (ww['principal'] + ww['interest']), np.nan).round(4)
-ww['prin_total'] = ww['principal'].cumsum()
+ww['prin_total'] = round(ww['principal'].cumsum(),2)
 
 # interest percentage and cumsum
 ww['int%'] =np.where(ww['type'] == 'payment', ww['interest'] / (ww['principal'] + ww['interest']), np.nan).round(4)
-ww['int_total'] = ww['interest'].cumsum()
+ww['int_total'] = round(ww['interest'].cumsum(),2)
 
 # running balance
-ww['balance'] = 500000 - ww['prin_total']
+ww['balance'] = round(500000 - ww['prin_total'], 2)
 
 ww["date"] = pd.to_datetime(ww["date"], format="%Y-%m-%d")
 types = ww["type"].unique()
@@ -51,12 +52,25 @@ colors = {
 }
 
 
-fig = px.line(ww, x="date", y="balance", color="type")
+fig = px.scatter(ww, x="date", y="balance", color="type")
 
 fig.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
     font_color=colors['text']
+)
+
+table = dash_table.DataTable(
+    data=ww.to_dict('records'),
+    columns=[{'id': c, 'name': c} for c in ww.columns],
+    fixed_rows={'headers': True},
+    style_as_list_view=True,
+    style_header={'backgroundColor': 'rgb(30, 30, 30)'},
+    style_cell={
+        'backgroundColor': 'rgb(50, 50, 50)',
+        'color': 'white',
+        'font-family': "Arial"
+    },
 )
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
@@ -76,7 +90,14 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     dcc.Graph(
         id='example-graph-2',
         figure=fig
-    )
+    ),
+
+    html.H4(children="Data table", style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    table
 ])
 
 if __name__ == '__main__':
